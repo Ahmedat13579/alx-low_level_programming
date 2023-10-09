787456d2e02a38939b5487d14710dd6d2c542099
Ahmedat13579 <duetogit@gmail.com>
2023-10-09 15:39:27 -0700

    hi

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,19 +10,21 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 	char CH = 'A';
 
         while (ch <= 'z')
-		{
+	{
 		putchar(ch);
 		ch++;
-	        }
+	}
+
 	while (CH <= 'Z')
-		{
+	{
 		putchar(CH);
-                CH++;
-        	}
+		CH++;
+        }
+
         putchar('\n');
         return (0);
 }
